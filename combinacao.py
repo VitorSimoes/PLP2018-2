cc4f41acefa368bb07d8975afa3694c3f2565941
silascastro <silascastro15@gmail.com>
2018-08-18 02:28:42 -0400

    combinacao_python

---
+++ combinacao.py
@@ -0,0 +1,14 @@
+#combinacao_
+
+
+def combinacao(e, n, k):
+    i = e+1
+    while(i<=n):
+        print(e,i)
+        i+=1
+
+n = int(input())
+k = int(input())
+
+for num in range(1,n+1): #para cada numero na sequencia de 1...n
+    combinacao(num, n, k) #faca (m,m+1)...(m,n) 

1c93aa9b22b456a97a1ed17bd1fbb6baec8cb9a1
NayaraCerdeira <naycerdeira@gmail.com>
2018-08-20 16:06:29 -0400

    Update permutacao.rb

--- permutacao.rb
+++ permutacao.rb
@@ -1,27 +1,35 @@
-n = gets.chomp.to_i
-i = 0
+def permuta(vetor,vetor_aux,cont)
+  tam = vetor.size
+  if cont == vetor.size
+      print(vetor_aux)
+  else
+    for i in 0 .. tam - 1
+      flag = false
+      for j in 0.. cont 
+        if(vetor_aux[j]==vetor[i])
+          flag = true
+        end
+      end
+      if(flag == false)
+        vetor_aux[cont]= vetor[i]
+        permuta(vetor,vetor_aux,cont+1)
+      end
+    end
+  end
 
-vetor = []
+end
+
+#main
+n = gets.chomp.to_i
+lista_aux=[]
+lista=[]
+i = 0
+j = 0
 
 while i < n do
-  vetor << i + 1
+  lista << i + 1
+  lista_aux<< 0
   i += 1
-  
 end
-
-def permutar(vetor)
-  return [vetor] if vetor.size < 2
-  perm = []
-
-  vetor.each{ |aux|
-    permutar(vetor - [aux]).each{|aux2|
-      perm << ([aux] + aux2)
-    }
-  }
-  
-  perm
-  
-end
-
-print permutar(vetor)
-
+    
+permuta(lista,lista_aux,0)
